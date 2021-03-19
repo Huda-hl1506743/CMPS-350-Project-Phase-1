3eea9093e24f6b2b372c5b2135306262672c0e63
Huda-hl1506743 <hl1506743@qu.edu.qa>
2021-03-19 21:54:26 +0200

    added check auth checking if user is already authenticated

--- js/login.js
+++ js/login.js
@@ -1,20 +1,51 @@
+import * as userRepository from './userRepository.js';
 
 
 const loginButton = document.querySelector("#loginButton");
-const emailError = document.querySelector("#emailError");
-const passwordError = document.querySelector("#passwordError");
-
+const emailSpan = document.querySelector("#emailError");
+const passwordSpan = document.querySelector("#passwordError");
+const loginSpan = document.querySelector("#loginError");
 
 //Event Listeners
 document.addEventListener("DOMContentLoaded", () => {
-
 });
 
+//Check if user is already logged in
+let CheckAuth = () => {
+    let loginStatus = userRepository.getLoginStatus();
+    if (loginStatus != false) {
+        //Check user type and redirect to his pages
+        if (loginStatus == 'principle')
+            window.location = '../index.html'
+    }
+};
 
 let login = () => {
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-    console.log(email, password);
+    //Remove Errors
+    loginSpan.innerText = '';
+    emailSpan.innerText = '';
+    passwordSpan.innerText = '';
+
+    let email = document.getElementById("email").value.trim();
+    let password = document.getElementById("password").value.trim();
+
+    //Check email and password
+    if (email.length === 0 || password.length === 0 || !email.match("[a-zA-Z0-9_]{3,}@[a-zA-Z0-9_]{3,}.[a-zA-Z0-9_]{2,4}")) {
+        if (email.length == 0 || !email.match("[a-zA-Z0-9_]{3,}@[a-zA-Z0-9_]{3,}.[a-zA-Z0-9_]{2,4}"))
+            emailSpan.innerText = 'Please Fill In The Email';
+        if (password.length == 0)
+            passwordSpan.innerText = 'Please Fill In a Password';
+    }
+    else {
+        let response = userRepository.login(email, password);
+        if (response.success != true) {
+            loginSpan.innerText = response.message;
+        } else {
+            window.location = '../index.html'
+        }
+    }
 }
 
 loginButton.addEventListener("click", login);
+
+CheckAuth();
