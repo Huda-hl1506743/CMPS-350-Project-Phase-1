456bdaa47464744c059518d6a8b079f1b0d3deb3
Huda-hl1506743 <hl1506743@qu.edu.qa>
2021-03-19 21:53:12 +0200

    added login that checks for user and authenticates him added checkfirstTime that checks if user has loaded the site for first time to load dummydata

--- js/userRepository.js
+++ js/userRepository.js
@@ -1,7 +1,37 @@
-export function getSession() {
-    return JSON.parse(localStorage.getItem('session'));
-}
-
 export function setSession(user) {
     localStorage.setItem('session', JSON.stringify(user));
+};
+
+export function login(email, password) {
+    let users = JSON.parse(localStorage.getItem('users'));
+    let value = {
+        success: false,
+        message: 'Invalid Credentials'
+    };
+    users.forEach(user => {
+        if (user.email === email && user.password === password) { value = { success: true, type: user.type, email: user.email }; return; }
+    });
+    if (value.success === true) {
+        setSession({
+            isLoggedIn: true,
+            email: value.email,
+            type: value.type
+        });
+    }
+    return value
+};
+
+export function getLoginStatus() {
+    let user = JSON.parse(localStorage.getItem('session'));
+    if (user)
+        return user.type;
+    return false;
+}
+
+export function firstTime() {
+    return localStorage.getItem("firstTime") ? true : false;
+}
+
+export function setFirstTime() {
+    localStorage.setItem('firstTime', JSON.stringify({ first: true }))
 }
