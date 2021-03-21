8d5eb20785bd65e4797b0743917b14dbe37cc191
AlAnoud AlMusleh <aa1407499@qu.edu.qa>
2021-03-21 18:40:33 +0200

    refactored code structure

---
+++ js/parent/buildPaymentHistory.js
@@ -0,0 +1,91 @@
+////////////////////////////////////////
+//Parents Payments History Builder
+
+function buildParentPaymentHistory(user, paymentRepository) {
+    let welcomeMessageDiv = document.querySelector('#welcome');
+    let payments = paymentRepository.getPaymentHistory();
+    payments = payments.filter((payment) => { return (payment.email === user.email) });
+
+    let TableRows = payments.map((payment) => `
+      <tr id="payment_${payment.id}">
+        <td>${payment.student}</td>
+        <td>${payment.type}</td>
+        <td>${payment.amount}</td>
+        <td>${payment.remaining}</td>
+        <td>${new Date(payment.date).toDateString()}</td>
+      </tr>
+    `);
+
+    welcomeMessageDiv.innerHTML = `
+    <h1 style="text-align: center; color: gray">
+                  Welcome To Payment Management
+                </h1>
+                <p style="text-align: center; color: black">
+                  Here is the list of your Pending Payments
+                </p>
+                <br /><br />
+    `;
+
+    bodyDiv.innerHTML = `
+    <div>
+      <div class="filter">
+        <label>Start Date:</label>
+        <input type="date" id="startDate">
+        <label>End Date:</label>
+        <input type="date" id="endDate"">
+        <a class="filter-btn" id="filter" style="cursor: pointer">Filter</a>
+      </div>
+      <table id="t01">
+        <tr>
+          <th>Student Name</th>
+          <th>Payment Type</th>
+          <th>Payed Amount</th>
+          <th>Remaining Amount</th>
+          <th>Date</th>
+        </tr>
+        <tbody id="tableBody">
+          ${TableRows.join('')}
+        <tbody>
+      </table>
+    </div>
+    `;
+
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    if (month < 10) month = "0" + month;
+    if (day < 10) day = "0" + day;
+
+    let today = year + "-" + month + "-" + day;
+
+    let startDate = document.getElementById('startDate');
+    startDate.value = today;
+    let endDate = document.getElementById('endDate');
+    endDate.value = today;
+
+    //Filter Function
+    document.querySelector('#filter').addEventListener('click', () => {
+        let sd = new Date(startDate.value).getTime();
+        //End date is the end of the day so 86394000 is 23 hours 59 minutes 59 seconds in milliseconds
+        let ed = new Date(endDate.value).getTime() + parseInt("86394000‬");
+
+        let payments = paymentRepository.getPaymentHistory();
+
+        payments = payments.filter(payment => {
+            let time = new Date(payment.date).getTime();
+            return (sd <= time && time < ed && payment.email === user.email);
+        });
+        let TableRows = payments.map((payment) => `
+        <tr id="payment_${payment.id}">
+          <td>${payment.student}</td>
+          <td>${payment.type}</td>
+          <td>${payment.amount}</td>
+          <td>${payment.remaining}</td>
+          <td>${new Date(payment.date).toDateString()}</td>
+        </tr>
+      `);
+        document.querySelector('#tableBody').innerHTML = TableRows.join('');
+    });
+}
